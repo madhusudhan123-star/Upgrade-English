1b474bac87fcd3449e8de5dc7a0f5bca132321ce
Madhusudhan <dmadhusudhan98@gmail.com>
2024-08-12 08:49:06 +0530

    ads

--- app/layout.js
+++ app/layout.js
@@ -60,6 +60,17 @@ export default function RootLayout({ children }) {
             style={{ display: 'none', visibility: 'hidden' }}
           ></iframe>
         </noscript>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4476899930876130"
+          crossorigin="anonymous"></script>
+        <ins class="adsbygoogle"
+          style="display:block"
+          data-ad-client="ca-pub-4476899930876130"
+          data-ad-slot="3533008443"
+          data-ad-format="auto"
+          data-full-width-responsive="true"></ins>
+        <script>
+          (adsbygoogle = window.adsbygoogle || []).push({ });
+        </script>
       </body>
     </html>
   );
